76718688fb4603ff0d3144e9f23e340555212927
Balazs Hajdu <bhajdu85@gmail.com>
2021-06-21 17:34:16 +0100

    files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import Movie from "./Movie/Movie";
+
+const api_key = "8dfdfd76c21d4d0227af54af4f2a59b7";
+
+const API = `https://api.themoviedb.org/3/search/movie?&api_key=${api_key}&query=`;
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(API + search)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <h1 className="header-h1">Movie Search</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search.."
+            onChange={handleChange}
+            value={search}
+            required
+          />
+          <input className="submit" type="submit" value="Search" />
+        </form>
       </header>
-    </div>
+
+      <div className="movie-container">
+        {movies.map((movie) => {
+          return <Movie key={movie.id} {...movie} />;
+        })}
+      </div>
+    </>
   );
 }
 
---
+++ src/Movie/Movie.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Movie.css";
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({
+  title,
+  poster_path,
+  overview,
+  release_date,
+  vote_average
+}) => (
+  <div className="movie flip-box">
+    <div className="flip-box-inner">
+      <div className="flip-box-front">
+        <img
+          src={
+            poster_path
+              ? IMG_API + poster_path
+              : "https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg"
+          }
+          alt={title}
+        />
+      </div>
+      <div className="flip-box-back">
+        <span className="vote_average">{vote_average}</span>
+        <h2>Overview:</h2>
+        <p>{overview}</p>
+        <p className="release_date">Release date: {release_date}</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Movie;
